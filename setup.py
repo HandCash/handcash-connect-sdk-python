3f4326c3a27c321daf9767b7959efcba33630427
Krzysztof Fonal <krzysiekfonal@gmail.com>
2020-11-09 11:57:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='handcash_connect_sdk',
-      version='0.1.0',
+      version='0.1.1',
       description='handcash_connect_sdk - library for interacting with Handcash Connect API',
       long_description='This library allows you to authorize with Handcash Cloud server and use Handcash Connect API '
                        'in order to get user\'s info and do payments on behalf of user\'s wallet.',
@@ -14,6 +14,7 @@ setup(name='handcash_connect_sdk',
       ],
       keywords='api handcash',
       url='https://github.com/krzysiekfonal/handcash-connect-sdk-python',
+      download_url='https://github.com/krzysiekfonal/handcash-connect-sdk-python/releases/tag/v0.1.1',
       author='Krzysztof Fonal',
       author_email='krzysiekfonal@gmail.com',
       license='The Unlicense',
