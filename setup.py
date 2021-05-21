bd91a4ebef32eadb4fb4631d7e17279dad6c49ee
Rafa JS <rjseibane@gmail.com>
2021-05-21 23:41:11 +0200

    Upgrade package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='handcash_connect_sdk',
-      version='0.2.0',
+      version='0.2.1',
       description='handcash_connect_sdk - library for interacting with Handcash Connect API',
       long_description='This library allows you to authorize with Handcash Cloud server and use Handcash Connect API '
                        'in order to get user\'s info and do payments on behalf of user\'s wallet.',
